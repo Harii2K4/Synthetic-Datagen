153f71dbc6bc11fefcc69301ca3c22badffab5a9
Harii2K4 <harikishan2k4@gmail.com>
2026-02-22 23:59:47 +0530

    modified setup.py to include saving model data

--- setup.py
+++ setup.py
@@ -6,8 +6,11 @@ Description: Setup file for the final project
 from datasets import load_dataset
 import os
 from pandas import DataFrame
+import requests
+import json
 #for downloading the dataset
 DATASET_FOLDER="./persona_hub/"
+MODEL_LIST_FILE="./openrouter_models_list.json"
 
 def storeAsCsv(datasetName:str):
     try:
@@ -29,20 +32,47 @@ def storeAsCsv(datasetName:str):
 
 def main():
         #creating the dataset folder if doesnt exist
-        os.makedirs(DATASET_FOLDER, exist_ok=True)
+        # os.makedirs(DATASET_FOLDER, exist_ok=True)
         #storing the general persona dataset
-        storeAsCsv("persona")
-
-        #storing the datasets for each domain
-        persona_domain_list=["math","instruction","knowledge","reasoning",
-                             "tool","npc"]
-        for domain in persona_domain_list:
-            try:
-                storeAsCsv(domain)
-            except Exception as e:
-                print(f"Error loading dataset: {e}")
-
+        # storeAsCsv("persona")
+        #
+        # #storing the datasets for each domain
+        # persona_domain_list=["math","instruction","knowledge","reasoning",
+        #                      "tool","npc"]
+        # for domain in persona_domain_list:
+        #     try:
+        #         storeAsCsv(domain)
+        #     except Exception as e:
+        #         print(f"Error loading dataset: {e}")
+        #
         print("Dataset saved to all domains successfully in persona_hub folder")
+       # List all models and their properties (GET /models)
+        response = requests.get(
+          "https://openrouter.ai/api/v1/models",
+          headers={},
+        )
+
+        if response.status_code != 200:
+            raise Exception(f"Error in downloading models information: {response.status_code}")
+        modelsData=response.json()
+        #filter the modelData to what we want
+        modelsData=[
+            {
+                "id":model.get('id'),
+                "name":model.get('name'),
+                "context_length":model.get('context_length'),
+                "input_price":model.get('pricing').get('prompt'),
+                "output_price":model.get('pricing').get('completion'),
+            }
+            for model in modelsData['data']
+            if 'text' in model.get('architecture').get('input_modalities') and
+            'text' in model.get('architecture').get('output_modalities')
+        ]
+        print(f'Storing {len(modelsData)} model information')
+
+        with open(MODEL_LIST_FILE, "w", encoding="utf-8") as f:
+            json.dump(modelsData,f,ensure_ascii=False,indent=2)
+
 
 if __name__ == "__main__":
     main()
